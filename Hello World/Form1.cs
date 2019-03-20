0da454b3ca9c084756d6813cf09c0f5a351a6892
Ibrahim <Avraham32@users.noreply.github.com>
2019-03-20 15:30:33 -0400

    Hello World Empty Project

---
+++ Hello World/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hello_World
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //TODO: Add your first void function and name it MyFirstFunction:
+
+        //TODO: Add your first variable of type string and name it msg:
+
+        //TODO: Create a MessageBox object to display the content of your msg variable:
+
+        //TODO: Link MyFirstFunction to the form's constructor method:
+
+
+    }
+}
